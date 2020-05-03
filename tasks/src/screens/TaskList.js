f35dc730d08ba7bbdd10d16de1de6765b4ea21e7
Samuel Dias <samuel.sdias@hotmail.com>
2020-05-03 17:36:37 -0300

    Primeiro commit

---
+++ tasks/src/screens/TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const TaskList = () => {
+    return (
+        <View>
+            <Text>TaskList</Text>
+        </View>
+    )
+}
+
+export default TaskList
